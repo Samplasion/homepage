916f95ecd51dc5a2b0ea687feecb38c7c2bd736c
Samplasion <dev@samplasion.js.org>
2020-04-03 15:00:14 +0200

    Commit @ 03/04/20 15:00:14

--- publi.sh
+++ publi.sh
@@ -5,12 +5,12 @@ f() {
     rm css/*.min.css css/*.min.css.map
     ./compile.sh css
     ./compile.sh js
-    
+
     echo
     echo '[COMMIT]'
     git add -A
     git add -f css/*.min.css
-    git commit -m "Commit @ `date +"%d/%m/%y @ %T"`"
+    git commit -m "Commit @ `date +"%d/%m/%y %T"`"
 
     echo
     echo '[PUSH]'
