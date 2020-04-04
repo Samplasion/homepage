0c715304ca282993afdc0f231bec27f57b60a66b
Samplasion <dev@samplasion.js.org>
2020-04-04 10:05:15 +0200

    Dark mode! Now fully functional and aesthetically pleasing to see. @ 04/04/20 10:05:15

--- sun.sh
+++ sun.sh
@@ -1,10 +1,27 @@
-echo "SUN - Website Tester\n"
-# 
-echo Compiles the required files and opens the webpage in a browser
-
 # ========= VARIABLES =========
 file="./index.html"
+OPEN=1
+
+for arg in "$@"
+do
+  case $arg in
+    -n|--no-open)
+    OPEN=0
+    shift # Remove argument name from processing
+    shift # Remove argument value from processing
+    ;;
+  esac
+done
 
 # =========== CODE ============
+echo "SUN - Website Tester\n"
+if [ ${OPEN} -lt 1 ]; then
+    echo Compiles the required files.
+else 
+    echo Compiles the required files and opens the webpage in a browser.
+fi
+
 ./compile.sh css js
-open $file
+if [ ${OPEN} = 1 ]; then
+    open ./index.html
+fi
